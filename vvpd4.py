dce8e380eeb3dee6af381639071435487523ad10
Artyom Vinokurov <artyom.vinokurov2001@gmail.com>
2021-12-23 00:17:26 +0700

    1st func

--- vvpd4.py
+++ vvpd4.py
@@ -1,3 +1,16 @@
+def syracuse_sequence(n):
+    p_list = list()
+    p_list.append(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n / 2
+            p_list.append(int(n))
+        else:
+            n = n * 3 + 1
+            p_list.append(int(n))
+    return p_list
+
+
 print('Write Number')
 number = int(input())
 print('Sequence - 1 or Max - 2')
