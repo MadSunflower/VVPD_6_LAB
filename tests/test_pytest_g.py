7522db907c9e153932aaa58859010027e339dcf9
Artyom Vinokurov <artyom.vinokurov2001@gmail.com>
2021-12-30 05:16:31 +0700

    ptst_2

--- tests/test_pytest_g.py
+++ tests/test_pytest_g.py
@@ -6,3 +6,23 @@ from ..sequence import syracuse_sequence, syracuse_max
 @pytest.fixture
 def test_data():
     return 1
+
+
+def test_loneliest_number(test_data):
+    assert syracuse_sequence(test_data) == [1]
+
+
+def test_one_max(test_data):
+    assert syracuse_max(test_data) == 1
+
+
+def test_big_number_max():
+    assert syracuse_max(12345) == 41668
+
+
+def test_zero_number():
+    assert syracuse_sequence(0) == []
+
+
+def test_default_number():
+    assert syracuse_sequence(13) == [13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
